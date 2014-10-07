428f31b59bcf1be51a97141eb6543e3d898c3b52
Abhi Sharma <pair+as@pivotallabs.com>
2014-10-07 18:19:54 -0400

    Add Gruntfile for public css/js compilation

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'vendor/assets/stylesheets',
+          src: ['*.sass'],
+          dest: 'public',
+          ext: '.css'
+        }]
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'sass']);
+
+};
